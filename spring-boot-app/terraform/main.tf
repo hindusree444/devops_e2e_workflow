d04aea9229ed8987eeb2bd35946cfd779d77a98f
hindu sree <hdunaboyina@softility.com>
2025-02-03 17:52:09 +0530

    adding files

---
+++ spring-boot-app/terraform/main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  credentials = file("devops-e2e-workflow-2f32ecf949ed.json")
+  project     = "devops-e2e-workflow"
+  region      = "us-central1"
+}
+
+resource "google_container_cluster" "gke_cluster" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "node-pool"
+  cluster    = google_container_cluster.gke_cluster.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 30
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
