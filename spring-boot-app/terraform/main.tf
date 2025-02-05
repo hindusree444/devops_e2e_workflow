2564e05b724270bb976a23191a12eab921f4cfd8
hindu sree <hdunaboyina@softility.com>
2025-02-05 13:12:47 +0530

    adding key

--- spring-boot-app/terraform/main.tf
+++ spring-boot-app/terraform/main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project = "devops-e2e-workflow"
-  region  = "asia-south1"
+  region  = "europe-west1"
 }
 
 resource "google_container_cluster" "gke_cluster" {
   name     = "my-gke-cluster"
-  location = "us-central1"
+  location = "europe-west1"
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -14,12 +14,12 @@ resource "google_container_cluster" "gke_cluster" {
 resource "google_container_node_pool" "primary_nodes" {
   name       = "node-pool"
   cluster    = google_container_cluster.gke_cluster.name
-  node_count = 2
+  node_count = 1
 
   node_config {
     service_account = "devops-e2e-sa@devops-e2e-workflow.iam.gserviceaccount.com"
     machine_type    = "e2-medium"
-    disk_size_gb    = 50
+    disk_size_gb    = 10
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
