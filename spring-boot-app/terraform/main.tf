a368323943b457da7e3b80e0226876d7bae91707
hindu sree <hdunaboyina@softility.com>
2025-02-05 16:20:53 +0530

    adding key

--- spring-boot-app/terraform/main.tf
+++ spring-boot-app/terraform/main.tf
@@ -11,7 +11,7 @@ resource "google_container_cluster" "gke_cluster" {
   remove_default_node_pool = true
   initial_node_count       = 1
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 }
 
@@ -30,7 +30,7 @@ resource "google_container_node_pool" "primary_nodes" {
   }
   lifecycle {
     # Remove prevent_destroy if present
-    prevent_destroy = true
+    prevent_destroy = false
   }  
 }
 
