f3ec1beb43aa18c02165f2a3f83df5824c56e9c8
Varaprasad0389 <varaprasad0389@gmail.com>
2025-02-17 15:15:18 +0530

    Update main.tf

--- spring-boot-app/terraform/main.tf
+++ spring-boot-app/terraform/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "my-vm-instance"
+  name         = "Sonarqube"
   machine_type = "e2-medium"
   zone         = "europe-west1-b"
 
@@ -18,4 +18,27 @@ resource "google_compute_instance" "vm_instance" {
     network = "default"
     access_config {}
   }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install docker.io -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+
+    # Kernel parameters for SonarQube
+    echo "vm.max_map_count=524288" >> /etc/sysctl.conf
+    echo "fs.file-max=131072" >> /etc/sysctl.conf
+    sysctl -p
+
+    echo "sonarqube - nofile 131072" >> /etc/security/limits.conf
+    echo "sonarqube - nproc 8192" >> /etc/security/limits.conf
+
+    sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:lts
+  EOT
+}
+
+output "sonarqube_url" {
+  value       = "http://${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}:9000"
+  description = "URL to access SonarQube after deployment"
 }
