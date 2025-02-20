0194a5753a2e3606dd91ec9373787d41eaaf2822
hindusree444 <134682745+hindusree444@users.noreply.github.com>
2025-02-20 13:19:36 +0530

    Delete spring-boot-app/terraform/variable.tf

--- spring-boot-app/terraform/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "google_credentials_json" {
-  description = "The path to the Google Cloud service account JSON credentials file."
-  type        = string
-  sensitive   = true  # Since this contains sensitive information, mark it as sensitive
-}
