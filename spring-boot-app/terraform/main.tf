875d06c01d086092f0d53c11d4e0f096dfc8bad8
hindusree444 <134682745+hindusree444@users.noreply.github.com>
2025-02-20 14:20:15 +0530

    Update main.tf

--- spring-boot-app/terraform/main.tf
+++ spring-boot-app/terraform/main.tf
@@ -1,12 +1,5 @@
 terraform 
 {
-  backend "gcs" {
-    bucket = "statestorebucket"  
-    prefix = "terraform/state"           
-    project = "devops-e2e-workflow"      
-    region  = "europe-west1"    
-    credentials = jsondecode(var.GOOGLE_CREDENTIALS_JSON)
-  }
   required_providers {
     google = {
       source = "hashicorp/google"
