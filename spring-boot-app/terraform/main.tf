afbbe72d81776da73c5d736a1913837f2ec21f27
hindusree444 <134682745+hindusree444@users.noreply.github.com>
2025-02-20 19:28:21 +0530

    Update main.tf

--- spring-boot-app/terraform/main.tf
+++ spring-boot-app/terraform/main.tf
@@ -4,6 +4,7 @@ terraform {
     prefix = "terraform/state"
   }
 }
+
 provider "google" {
   project = "devops-e2e-workflow"  # Replace with your Google Cloud project ID
   region  = "europe-west1"          # Your preferred region
@@ -34,9 +35,29 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+    
+    tags = ["gke-cluster"]  # Adding network tags to nodes
   }
 
   lifecycle {
     prevent_destroy = false
   }
 }
+
+# Firewall rule to allow HTTP, HTTPS, and SSH traffic to GKE nodes
+resource "google_compute_firewall" "allow_http_https_ssh" {
+  name    = "allow-http-https-ssh"
+  network = "default"  # Specify the network where the GKE cluster is created (default is "default")
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "22"]  # Allow traffic on HTTP (80), HTTPS (443), and SSH (22)
+  }
+  
+  source_ranges = ["0.0.0.0/0"]  # Allow traffic from all sources (any IP)
+  direction     = "INGRESS"      # Incoming traffic
+  priority      = 1000           # Priority (lower numbers are higher priority)
+  
+  target_tags = ["gke-cluster"]  # Apply this firewall rule to nodes with the "gke-cluster" tag
+}
+
